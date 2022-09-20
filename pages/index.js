f38dda42d4bf72ee7b2ac557152e38b2e4de35bb
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-09-20 21:57:44 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,14 @@ import { Product, FooterBanner, HeroBanner } from "../components";
 
 const Home = ({ products, bannerData }) => (
   <div>
-    <HeroBanner />
-    {console.log(bannerData)}
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
     <div className="products-heading">
-      <h2>Best Selling Products</h2>
+      <h2>Best Seller Products</h2>
       <p>Speakers of many variations</p>
     </div>
 
     <div className="products-container">
-      {products?.map((product) => product.name)}
+      {products?.map((product) => <Product key={product._id} product={product} />)}
     </div>
 
     <FooterBanner />
